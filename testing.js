760fae3064f9f9a4c02e1cab4b750cd2dec56b9f
Nick Rockford <nwoffard@cnm.edu>
2022-07-18 14:35:19 -0600

    added estebans lecture from 7/18 and added to the style sheet and included a picture

---
+++ testing.js
@@ -0,0 +1,13 @@
+
+
+function spinWords ({ firstWord }){
+  if firstWord.length <= 5 {
+  return console.log(firstWord);
+  }
+
+
+
+}
+let firstWord = "hello";
+
+spinWords({ firstWord: firstWord });
